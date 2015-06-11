a9372f393777e990b3644971b1f9fd81047a8766
info206 <info206@LAB02-04.edupit.intra>
2015-06-11 17:07:17 -0300

    inicial

---
+++ src/Model/Cliente.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+public class Cliente {
+    String endereco, nome, cpf, email, rg, telefone;
+
+    public Cliente(String endereco, String nome, String cpf, String email, String rg, String telefone) {
+        this.endereco = endereco;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.rg = rg;
+        this.telefone = telefone;
+    }
+   
+   
+   
+   }
